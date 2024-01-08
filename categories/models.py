ec259df4d76eecbb20eec80e682bccd841fe5800
azizxojaev <azizxojaevbexruz@gmail.com>
2024-01-08 19:13:25 +0500

    added 5 models

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class MainCategory(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(MainCategory, on_delete=models.CASCADE)
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
