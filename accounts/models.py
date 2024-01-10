a3639482fc9a06dd1d2c388e818f6ad3943d89fa
Winchestor2001 <sbexruz2001@gmail.com>
2024-01-10 17:23:03 +0500

    update code

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     photo = models.ImageField('profile/', blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    email = models.CharField(max_length=50, blank=True, null=True)
+    full_name = models.CharField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
